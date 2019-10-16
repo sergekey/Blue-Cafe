a63d6cdd58c2cd02eba46300cb6490d606d6519f
sergekey <sergekey@gmail.com>
2019-10-16 18:31:23 +0300

    some changes in markup

--- menu_tabs.js
+++ menu_tabs.js
@@ -1,5 +1,5 @@
 function choose_menu(evt, menu) {
-    var i, tabcontent, tablinks;
+    let i, tabcontent, tablinks;
 
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
